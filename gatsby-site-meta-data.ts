0afb1c04c94639f3cbaf7bffd5088af3ec577d6e
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-03-22 21:09:14 +0900

    fix: 메타데이터 변경

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -2,10 +2,10 @@ export default {
   /**
    * basic Information
    */
-  title: `jeong-min.com`,
-  description: `개발자 단민`,
+  title: `myeong-uk.com`,
+  description: `개발자 이명욱`,
   language: `ko`,
-  siteUrl: `https://jeong-min.com/`,
+  siteUrl: `https://myeonguk.netlify.app/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
 
   /**
@@ -13,7 +13,7 @@ export default {
    */
   comments: {
     utterances: {
-      repo: ``, //`danmin20/danmin-gatsby-blog`,
+      repo: `myeonguklee/myeonguk-gatsby-blog`, //`danmin20/danmin-gatsby-blog`,
     },
   },
 
@@ -21,22 +21,23 @@ export default {
    * introduce yourself
    */
   author: {
-    name: `이정민`,
-    nickname: `단민`,
+    name: `이명욱`,
+    nickname: `명욱`,
     stack: ['Frontend', 'React', 'Typescript'],
     bio: {
-      email: `ljm991108@gmail.com`,
+      email: `muk525@icloud.com`,
       residence: 'Seoul, South Korea',
-      bachelorDegree: 'Kyunghee Univ. Computer Engineering (2018.03-2022.02)',
+      bachelorDegree: '서울교육대학교 초등교육과(심화 전공:컴퓨터) (2016.03-2022.02)',
+      // bachelorDegree: 'Kyunghee Univ. Computer Engineering (2018.03-2022.02)',
     },
     social: {
-      github: `https://github.com/danmin20`,
-      linkedIn: `https://www.linkedin.com/in/jeonming-lee/`,
-      resume: `https://www.figma.com/file/dtkCl6G7G5DVe18DN2LWny/%EC%9D%B4%EC%A0%95%EB%AF%BC-%EC%9D%B4%EB%A0%A5%EC%84%9C?node-id=0%3A1`,
+      github: `https://github.com/myeonguklee`,
+      linkedIn: `https://www.linkedin.com/in/myeong-uk/`,
+      resume: `https://my.surfit.io/w/1344468926`,
     },
     dropdown: {
-      tistory: 'https://danminblog.tistory.com/',
-      velog: 'https://velog.io/@danmin20',
+      tistory: '',
+      velog: '',
     },
   },
 
@@ -45,12 +46,12 @@ export default {
    */
   featured: [
     {
-      title: 'category1',
-      category: 'featured-category1',
+      title: 'dev',
+      category: 'featured-dev',
     },
     {
-      title: 'category2',
-      category: 'featured-category2',
+      title: 'basic',
+      category: 'featured-basic',
     },
   ],
 
@@ -58,28 +59,28 @@ export default {
    * metadata for About Page
    */
   timestamps: [
-    {
-      category: 'Career',
-      date: '2022.01.04 - NOW',
-      en: 'A Corp.',
-      kr: 'A 회사',
-      info: 'A 팀',
-      link: '',
-    },
-    {
-      category: 'Career',
-      date: '2021.01.04 - 2022.01.04',
-      en: 'B Corp.',
-      kr: 'B 회사',
-      info: 'B 팀',
-      link: '',
-    },
+    // {
+    //   category: 'Career',
+    //   date: '2022.01.04 - NOW',
+    //   en: 'A Corp.',
+    //   kr: 'A 회사',
+    //   info: 'A 팀',
+    //   link: '',
+    // },
+    // {
+    //   category: 'Career',
+    //   date: '2021.01.04 - 2022.01.04',
+    //   en: 'B Corp.',
+    //   kr: 'B 회사',
+    //   info: 'B 팀',
+    //   link: '',
+    // },
     {
       category: 'Activity',
-      date: '2023.07 - NOW',
-      en: 'Community',
-      kr: '커뮤니티',
-      info: 'IT 커뮤니티',
+      date: '2024.01 - 2024.12',
+      en: 'SSAFY',
+      kr: '삼성 청년 SW 아카데미',
+      info: '11기 수료',
       link: '',
     },
   ],
@@ -89,14 +90,27 @@ export default {
    */
   projects: [
     {
-      title: 'Portfolio',
-      description: '포트폴리오',
-      techStack: ['React', 'Next.js', 'Typescript'],
-      thumbnailUrl: '', // Path to your in the 'assets' folder
+      title: 'NewStock',
+      description: '경제 뉴스로 투자 안목을 키우고, 모의 투자로 실전 감각까지 다지는 빅데이터 플랫폼',
+      techStack: ['React', 'Typescript'],
+      thumbnailUrl: 'newstock.png', // Path to your in the 'assets' folder
       links: {
         post: '',
-        github: '',
+        github: 'https://github.com/myeonguklee/newstock',
         demo: '',
+        googlePlay: '',
+        appStore: '',
+      },
+    },
+    {
+      title: '공기밥',
+      description: '공공기관 업무추진비 기반 맛집 추천 서비스',
+      techStack: ['React', 'Next.js', 'Typescript'],
+      thumbnailUrl: 'gonggibap.png', // Path to your in the 'assets' folder
+      links: {
+        post: '',
+        github: 'https://github.com/myeonguklee/gonggibap-client',
+        demo: 'https://gonggibap.co.kr/',
         googlePlay: '',
         appStore: '',
       },
